02d63d8fb36d81b26daadb6e09f65ba306028ffd
Aaron Nech <necha@cs.washington.edu>
2015-02-07 12:06:27 -0800

    added filter stuff

---
+++ src/client/component/SourceFilterComponent.jsx
@@ -0,0 +1,90 @@
+var React = require('react');
+var Filter = require('../../common/model/Filter');
+
+/**
+ * Encapsulates the SourceFilterComponent of the application
+ */
+var SourceFilterComponent = React.createClass({
+
+    /**
+     * Initialize
+     */
+    componentDidMount : function() {
+        
+    },
+
+    /**
+     * Called when the component updates
+     */
+    componentDidUpdate: function() {
+        this.props.onChangeFilter(this);
+    },
+
+    /**
+     * Get the initial state
+     */
+    getInitialState : function() {
+        return {
+            disabled : !this.props.API.isFilterEnabled(this.getName())
+        };
+    },
+
+    /**
+     * Get if this filter is enabled or not
+     * @return {boolean} True if enabled, false otherwise
+     */
+    getEnabled : function() {
+        return !this.state.disabled;
+    },
+
+    /**
+     * Gets the name of this filter
+     * @return {string} the name
+     */
+    getName : function() {
+        return "source-" + this.props.filterId;
+    },
+
+    /**
+     * Gets the filter for this filter component
+     * @return {Filter} the filter
+     */
+    getFilter : function() {
+        return new Filter(function(json, value) {
+            return json.source != value;
+        }, this.props.source, this.state.disabled);
+    },
+
+    /**
+     * Called when the checkbox changes state
+     */
+    onCheckBoxChange : function() {
+        this.setState({disabled : !this.state.disabled});
+    },
+
+	/**
+	 * Render the component
+	 */
+    render : function() {
+        return (
+	        <div className={"filter location-filter " + (this.state.disabled ? 'disabled' : 'enabled')}>
+                <fieldset>
+                    <legend>Data Source - {this.props.displayName}</legend>
+                    <div className="left">
+                        <input type="checkbox" name="enabled" onChange={function(){}} checked={!this.state.disabled}>
+                        </input>
+                        <label htmlFor="enabled" onClick={this.onCheckBoxChange}>
+                            <span className="ui"></span>
+                        </label>
+                    </div>
+                    <div className="right">
+                        <p>{this.props.displayName}</p>
+                    </div>
+                    <div style={{clear: 'both'}}></div>
+                </fieldset>
+	        </div>
+        );
+	}
+});
+
+module.exports = SourceFilterComponent;
---
+++ src/server/util/ProfileRecommender.ts
@@ -0,0 +1,31 @@
+import Profile = require('../../common/model/Profile');
+
+/**
+ * A ProfileRecommender generates recommendations to notify the user
+ * based on their profile.
+ */
+class ProfileRecommender {
+	private static MYPLAN_THRESHOLD : number = 2000;
+
+	constructor() {}
+
+	/**
+	 * Generates an appropriate notification for the given profile
+	 * @param  {Profile} profile The user profile
+	 * @return {string} The recommendation. Null if none given
+	 */
+	public generate(profile : Profile) : string {
+		console.log("Recommending...");
+		if (!profile) return null;
+
+		console.log("Profile salary: " + profile.getSalary());
+		if (profile.getSalary() < ProfileRecommender.MYPLAN_THRESHOLD) {
+			console.log("Low income match");
+			return "Your income may qualify you for MyPlan. <a href=\"data.gov\">Learn more</a>";
+		}
+
+		return null;
+	}
+}
+
+export = ProfileRecommender;
